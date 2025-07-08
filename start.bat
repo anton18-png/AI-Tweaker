48f3c7636c2b294e22580bd642d63776d8c27629
Solomon Chik <86909941+anton18-png@users.noreply.github.com>
2025-07-08 11:07:38 +0300

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,1 +1,23 @@
+@echo off
+
+:: Укажите путь к 7za.exe, если он не находится в переменной PATH
+set "sevenZipPath=7za.exe"  
+set "archivePath=tweaks.7z"
+set "targetFolder=Brian"
+
+:: Проверка наличия папки Brian
+if exist "%targetFolder%" (
+    echo Папка "%targetFolder%" уже существует. Архив не будет распакован.
+    exit /b
+)
+
+:: Проверка наличия файла tweaks.7z
+if exist "%archivePath%" (
+    echo Распаковка архива %archivePath%...
+    "%sevenZipPath%" x "%archivePath%"
+    echo Архив успешно распакован.
+) else (
+    echo Файл %archivePath% не найден.
+)
+
 launcher.exe main.exe
